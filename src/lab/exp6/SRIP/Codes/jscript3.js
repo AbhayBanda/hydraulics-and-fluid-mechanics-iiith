288e13a706f90f228d9d5b1d56067de3885f5ccb
AbhayBanda <bandaabhay@gmail.com>
2019-06-03 15:26:12 +0530

    3d experiment layout

---
+++ src/lab/exp6/SRIP/Codes/jscript3.js
@@ -0,0 +1,5 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.font = "20px Bold";
+ctx.fillStyle = "yellow";
+ctx.fillText("Orifice Experiment", 250, 70);
---
+++ src/lab/exp6/SRIP/Codes/jscript4.js
@@ -0,0 +1,7 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "#ffffff";
+ctx.fillRect(15,80,670,500);
+canvas.style.position = "absolute";
+canvas.style.left = "300px";
+canvas.style.top = "30px";
