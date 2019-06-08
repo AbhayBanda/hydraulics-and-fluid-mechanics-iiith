b1f82fe7f51ef799be30c20e2cddcfebc57bbfeb
AbhayBanda <bandaabhay@gmail.com>
2019-06-08 13:43:27 +0530

    Unnecessary files removed and Input form Fixed

--- src/lab/exp6/SRIP/Codes/jscript2.js
+++
@@ -1,7 +0,0 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-ctx.fillStyle = "#4C4C4C";
-ctx.fillRect(0,50,700,600);
-canvas.style.position = "absolute";
-canvas.style.left = "300px";
-canvas.style.top = "30px";
--- src/lab/exp6/SRIP/Codes/jscript3.js
+++
@@ -1,5 +0,0 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-ctx.font = "20px Bold";
-ctx.fillStyle = "yellow";
-ctx.fillText("Orifice Experiment", 250, 70);
